f5d05b41b2af74cbbbf0b176713b5d047133e79d
manatsananthepkaew <122779998+manatsananthepkaew@users.noreply.github.com>
2023-09-04 17:05:39 +0000

    done

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -7,20 +7,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//TODO: add webservlet to "/calbmi"
+
 
 @WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
      @Override
      protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
         double weight = Double.parseDouble(request.getParameter("weight"));
         double height = Double.parseDouble(request.getParameter("height"));
-        //TODO: calculate bmi
         double bmi = weight / ((height ) * (height ));
         int roundedBMI = (int) Math.round(bmi);
-        
-//TODO: determine the built from BMI
+
+
         String builtType ="normal";
         if (bmi < 18.5) {
             builtType = "underweight";
@@ -34,13 +32,13 @@ public class BMICalculatorServlet extends HttpServlet{
             builtType = "extremely obese";
         }
       
-        //TODO: add bmi and built to the request's attribute
+
         request.setAttribute("bmi", roundedBMI);
         request.setAttribute("builtType", builtType);
 
         
 
-        //TODO: forward to jsp
+
         request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
            
     }
